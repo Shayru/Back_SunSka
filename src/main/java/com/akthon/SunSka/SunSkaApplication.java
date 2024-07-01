fc16cbbb2b3f03337ba943dcb76b32c30717a71a
Shayruna <whitedarkai@gmail.com>
2024-07-01 16:02:38 +0200

    base

---
+++ src/main/java/com/akthon/SunSka/SunSkaApplication.java
@@ -0,0 +1,13 @@
+package com.akthon.SunSka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunSkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunSkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akthon/SunSka/SunSkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akthon.SunSka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunSkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
